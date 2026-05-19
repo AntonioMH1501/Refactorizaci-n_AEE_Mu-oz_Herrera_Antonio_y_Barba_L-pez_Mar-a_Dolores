c34a9c321053bb94f550d3f619785e7d610eebae
AntonioMH1501 <antoniomh1501@gmail.com>
2026-05-19 13:43:13 +0200

    Add files via upload

---
+++ FacturacionLegacyTest.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author DAM1
+ */
+public class FacturacionLegacyTest {
+    
+    public FacturacionLegacyTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
